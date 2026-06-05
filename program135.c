fc94279a56ebf4099c6fd698612d9fdd06881192
shreya2004 <kulkarnishreya971@gMAIL.com>
2026-06-05 21:02:48 +0530

    C program for maximum and minimum from array

---
+++ program135.c
@@ -0,0 +1,53 @@
+/*
+Take N numbers and return maximum element from array
+
+time complexity : O(N)
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Maximum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMax = 0;
+
+    iMax = Arr[0];
+
+    for(iCnt = 0; iCnt <iSize; iCnt ++)
+    {
+        if(Arr[iCnt] > iMax)
+        { 
+            iMax = Arr[iCnt];
+        }
+    }
+
+    return iMax;
+}
+int main()
+{
+    int iLength = 0, iCnt =0;
+    int iRet = 0;
+    int * Brr = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter elements : \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Maximum(Brr, iLength);
+
+    printf("Maximum element is : %d",iRet);
+
+    free(Brr);
+
+    return 0;
+}
---
+++ program136.c
@@ -0,0 +1,53 @@
+/*
+Take N numbers and return minimum element from array
+
+time complexity : O(N)
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int Minimum(int Arr[], int iSize)
+{
+    int iCnt = 0;
+    int iMin = 0;
+
+    iMin = Arr[0];
+
+    for(iCnt = 0; iCnt <iSize; iCnt ++)
+    {
+        if(Arr[iCnt] < iMin)
+        { 
+            iMin = Arr[iCnt];
+        }
+    }
+
+    return iMin;
+}
+int main()
+{
+    int iLength = 0, iCnt =0;
+    int iRet = 0;
+    int * Brr = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d",&iLength);
+
+    Brr = (int *)malloc(iLength * sizeof(int));
+
+    printf("Enter elements : \n");
+
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        scanf("%d",&Brr[iCnt]);
+    }
+
+    iRet = Minimum(Brr, iLength);
+
+    printf("Minimum element is : %d",iRet);
+
+    free(Brr);
+
+    return 0;
+}
