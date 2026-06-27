b7d793948b1740fa890b9696c97c801d4837dad7
shreya2004 <kulkarnishreya971@gMAIL.com>
2026-06-27 09:23:43 +0530

    Codes on biwise Operator for manual shift

---
+++ LB_Programs/program320.c
@@ -0,0 +1,18 @@
+/*
+Decimal Hexadecimal table till 100 decimal
+*/
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+int main()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 100; iCnt++)
+    {
+        printf("%d\t%X\n",iCnt,iCnt);
+    }
+
+    return 0;
+}
